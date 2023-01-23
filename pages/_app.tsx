5dfe2cc18d72260e106f7cb1f1509c1c6277aa17
Shie1 <sonkolyw@gmail.com>
2023-01-23 16:10:23 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider, Container } from "@mantine/core"
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<MantineProvider withGlobalStyles withNormalizeCSS theme={{
+    colorScheme: 'dark',
+    primaryColor: 'violet',
+  }}>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <div style={{ display: 'flex', alignItems: 'center', minHeight: '100vh' }}>
+      <Container sx={(theme) => ({ boxShadow: `0px 0px 5px 2px ${theme.fn.rgba(theme.colors.dark[7], .6)}`, backgroundColor: theme.fn.rgba(theme.colors.dark[7], .6) })} p="md">
+        <Component {...pageProps} />
+      </Container>
+    </div>
+  </MantineProvider>)
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name='keywords' content="music, zene, magyar, alter, alternatív, indie" />
+      </Head>
       <body>
         <Main />
         <NextScript />
