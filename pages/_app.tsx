da1c5bc3aa0f222e8cf0e557c59624f8dbe64ccf
Shie1 <sonkolyw@gmail.com>
2023-02-04 14:35:53 +0100

    added margin to footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     </Head>
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Component {...pageProps} />
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="mt-8 flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
